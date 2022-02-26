82a405b94dd19711aaf9d92c5d7ad44a093e76c8
Minh tu Huynh <tranm@MacBook-Pro-3.local>
2022-02-25 16:50:09 -0800

    added new pages

--- src/App.js
+++ src/App.js
@@ -3,21 +3,41 @@ import './App.css';
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+export default function Router() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}/>
+      <Route path="/resources" element={<div>Hello WOrld!</div>}/>
+      <Route path="/statistics" element={<div>Hello WOrld!</div>}/>
+      <Route path="/:home" element={<App />}/>
+    </Routes>
+  </BrowserRouter>;
+}
 
 
 function App() {
   return (
     <div className="App">
       <header className="header">
-      <div className="links">
-        <span className="title">Statistics</span>
-        <span className="title">Communities</span>
-        <span className="title">Resources</span>
+      {/* creating nav links */}
+      <div className="navBar">
+        <Link to="/statistics">Statistics</Link>
+        <Link to="/">Communities</Link>
+        <Link to="/resources">Resources</Link>
       </div>
       <div className='name'>
       </div>
     </header>
 
+{/* creating buttons */}
     <div className='community'>
       <Button variant="contained">Mental Health</Button>
       <Button variant="contained">Sex Education</Button>
@@ -31,4 +51,3 @@ function App() {
   );
 }
 
-export default App;
